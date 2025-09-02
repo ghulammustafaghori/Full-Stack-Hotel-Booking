f5ced39012e5be209725669d32e15ccf55ca51b8
Ghulam Mustafa <ghulammustafaghori312484913@gmail.com>
2025-09-02 09:19:22 +0500

    set clerk for backend

--- server/server.js
+++ server/server.js
@@ -4,12 +4,18 @@ const cors= require('cors');
 const dotenv= require('dotenv');
 const connectDB= require('./configs/db.js');
 
+const {clerkMiddleware} = require('@clerk/express');
+
 dotenv.config();
 
 connectDB();
 
 const app=express();
 app.use(cors()); // Enable Cross-Origin Resource Sharing (CORS)
+
+//Middleware
+app.use(express.json());
+app.use(clerkMiddleware())
 
 app.get('/', (req,res)=>{
     res.send('API is working fine')
